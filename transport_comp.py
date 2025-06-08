667a3bc82189599dff8696ff543b761f0ef59551
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-06-07 23:53:59 -0400

    Create transport_comp.py

---
+++ transport_comp.py
@@ -0,0 +1,14 @@
+# Initialize variables
+has_license = True
+has_space = True
+has_experience = False 
+
+# Calculate conditions
+can_sell_regular_pet = (has_license == True) or (has_experience == True) and (has_space == True)
+can_sell_exotic_pet = (has_license == True) and (has_experience == True) and (has_space == True)
+cannot_sell_any_pet = (has_license == False) and (has_experience == False) or (has_space == False)
+
+# Don't delete the lines below
+print("Can sell regular pet:", can_sell_regular_pet)
+print("Can sell exotic pet:", can_sell_exotic_pet)
+print("Cannot sell any pet:", cannot_sell_any_pet)
