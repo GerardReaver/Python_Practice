bf894f69b9e5f28e6fdaf13b1703ba5938698200
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-06-17 17:40:31 -0400

    Create resturn_functions.py

---
+++ resturn_functions.py
@@ -0,0 +1,8 @@
+# Create a function called "square_number" that takes a single paramenter "n" and returns its square. 
+# Then, call teh function with the input value and store the result in a variable called "result". 
+# Finally, print the value of "result". 
+def square_number(n):
+    print(n * n)
+n = int(input())
+
+square_number(n)
