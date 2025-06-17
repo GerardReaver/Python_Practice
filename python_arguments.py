6753d23fe62d42112bcb5f2189b4f712b9ca5ccb
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-06-17 17:23:49 -0400

    Create python_arguments.py

---
+++ python_arguments.py
@@ -0,0 +1,11 @@
+# Write a program that takes two numbers as input. These input numbers will be used as arguments for the function described below. 
+# Create a function that gets two arguemts, calculates their product, and prints the result. You can name the function as you like. 
+
+var1 = int(input())
+var2 = int(input())
+total = 0
+
+def prod_arg(var1, var2):
+    print(var1 * var2)
+
+prod_arg(var1, var2)
