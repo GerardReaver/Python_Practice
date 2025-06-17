af456bbbc37f70f41d733e394a7d9ba1589a0afe
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-06-17 17:29:46 -0400

    Create master_arguments.py

---
+++ master_arguments.py
@@ -0,0 +1,10 @@
+# define a function that accepts two arguments (length and width), calculates the area of the rectanle (length * width), and prints the result. 
+# After definint the function, prompt the user for the length and the width inputs and then call your function with these inputs. 
+def calculate_area(length, width):
+    # Write your code below
+    print(length * width) 
+
+length = float(input())
+width = float(input())
+# Call the function below
+calculate_area(length, width)
