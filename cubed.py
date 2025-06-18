c625e9d6d912838a298f20d5e6549805fbb09575
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-06-18 16:46:56 -0400

    Create cubed.py

---
+++ cubed.py
@@ -0,0 +1,10 @@
+# Create a function called cube_number that takes a single parameter n and returns its cube. 
+# Then, call the function with the input value and store the result in a variable called result. Finally, print the value of result.
+def cube_number(n):
+    # Write you code below
+    return n * n * n 
+    
+
+input_num = int(input())
+result = cube_number(input_num)  # call the function
+print(result)
