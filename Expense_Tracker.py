afa1be8f8c972d7818024209e6fb27e39f0c9f0c
Gerard <105612079+GerardReaver@users.noreply.github.com>
2025-10-12 13:17:53 -0400

    Create Expense_Tracker.py

---
+++ Expense_Tracker.py
@@ -0,0 +1,17 @@
+# This will be a mini project to keep track of Daily Expenses. 
+
+print("Welcome to the Daily Expense Tracker!")
+print("")
+print("Menu:")
+print("1. Add a new expense")
+print("2. View all expenses")
+print("3. Calculate total and average expense")
+print("4. Clear all expenses")
+print("5. Exit")
+
+while True:
+  if choice == 5:
+    print("Exiting the Daily Expense Tracker. Goodbye!")
+    break
+  elif choice != 5:
+    continue
